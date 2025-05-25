1941509e7ab48660d63e5898da9a2d1747ef73e1
abuyusufrony <abuyusufrony@gmail.com>
2025-05-26 00:33:39 +0600

    dynamic-added

--- src/app/components/Blogs/[id]/page.jsx
+++ src/app/components/Blogs/[id]/page.jsx
@@ -1,15 +1,29 @@
 import SingleAr from '@/lib/SingleAr';
 import React from 'react';
+import Tab from '../../Tab/Tab';
 
 async function page({ params }) {
     const { id } = params
     const sa = await SingleAr(id)
 
     return (
-        <h2>
-            this are dynamic page
-            <h2>title : {sa.title} </h2>
-        </h2>
+
+        <div className="max-w-2xl px-6 py-16 mx-auto space-y-12">
+            <article className="space-y-8 dark:bg-gray-100 dark:text-gray-900">
+                <div className="space-y-6">
+                    <h1 className="text-4xl font-bold md:tracking-tight md:text-5xl">{sa.title}</h1>
+                    <div className="flex flex-col items-start justify-between w-full md:flex-row md:items-center dark:text-gray-600">
+                        <div className="flex items-center md:space-x-2">
+                            <img src="https://source.unsplash.com/75x75/?portrait" alt="" className="w-4 h-4 border rounded-full dark:bg-gray-500 dark:border-gray-300" />
+                            <p className="text-sm">{sa.public_reactions_count} Reactions •  Post Date {new Date(sa.published_at).toLocaleDateString()} </p>
+                        </div>
+                        <p className="flex-shrink-0 mt -3 text-sm md:mt-0">{sa.reading_time_minutes} min read • {sa.comments_count} Comments</p>
+                    </div>
+                    <Tab></Tab>
+                </div>
+            </article>
+        </div>
+
     );
 };
 
---
+++ src/app/components/Tab/Tab.jsx
@@ -0,0 +1,30 @@
+"use client"
+import Link from 'next/link';
+import React, { useState } from 'react';
+
+const Tab = () => {
+    const [tabindex, settabindex] = useState(false)
+
+
+    return (
+        <div className="flex items-center -mx-4 overflow-x-auto overflow-y-hidden sm:justify-center flex-nowrap dark:bg-gray-100 dark:text-gray-800">
+            <Link onClick={() => settabindex(1)} rel="noopener noreferrer" href={'#'} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabindex === 1 ? "border border-b-0" : "border-b"} dark:border-gray-600 dark:text-gray-600`}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
+                    <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+                </svg>
+                <span>Architecto</span>
+            </Link>
+            <Link onClick={() => settabindex(0)} rel="noopener noreferrer" href={'#'} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabindex === 0 ? "border border-b-2" : "border-b"} dark:border-gray-600 dark:text-gray-600`}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
+                    <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
+                    <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
+                </svg>
+                <span>Corrupti</span>
+            </Link>
+
+
+        </div>
+    );
+};
+
+export default Tab;
